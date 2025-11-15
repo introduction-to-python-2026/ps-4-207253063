96faa47141228c17a54ddf78dffe8b3a32cc3bb0
207253063 <gal.zuk@live.biu.ac.il>
2025-11-15 13:50:56 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -1,6 +1,25 @@
 def split_before_each_uppercases(formula):
-    pass # Replace the `pass` with your code
+  start = 0
+  end = 1
+  split_formula = []
+  for char in formula[1:]:
+    if char.isupper():
+      split_formula.append(formula[start:end])
+      start = end
+    end += 1
+  split_formula.append(formula[start:])
+  return split_formula
 
 
 def split_at_first_digit(formula):
-    pass # Replace the `pass` with your code
+    digit_location =1
+    for i, my_char in enumerate(formula[1:]):
+      if my_char.isdigit():
+        break
+      digit_location += 1
+    if digit_location == len(formula):
+      return (formula, 1)
+    else:
+      prefix = formula[:digit_location]
+      number = int(formula[digit_location:])
+      return (prefix, number)
